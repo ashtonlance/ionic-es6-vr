be701d73fac9c4568661eab48e52279e894b2e64
Ashton Lance <ashton@ashtonlance.com>
2017-06-19 16:03:48 -0400

    Back button overlay
    iPad pro is still sucky

--- src/app/views/video/video.controller.js
+++ src/app/views/video/video.controller.js
@@ -11,37 +11,47 @@ export default class VideoController {
     $scope.$on('$ionicView.beforeEnter', function() {
       console.log('entering');
       screen.orientation.lock('landscape');
+      if (window.StatusBar && StatusBar.isVisible) {
+        StatusBar.hide();
+      }
     });
     $scope.$on('$ionicView.beforeLeave', function() {
       console.log('leaving');
-      var oldPlayer = document.getElementById('videojs-panorama-player');
+      let oldPlayer = document.getElementById('videojs-panorama-player');
       videojs(oldPlayer).dispose();
       screen.orientation.lock('portrait');
+      if (window.StatusBar && StatusBar.isVisible) {
+        StatusBar.show();
+      }
     });
     let source = localStorage.getItem('VideoSource');
 
     // screen.orientation.addEventListener('change', function() {
-      var that = this;
-      setTimeout(function() {
-        var videoElement = document.getElementById('videojs-panorama-player');
-        var width = window.innerWidth;
-        var height = window.innerHeight;
-        that.player = videojs(videoElement, {}, () => {});
-        that.player.src({ src: source, type: 'video/mp4' });
-        that.player.width(width), that.player.height(height);
+    var that = this;
+    setTimeout(function() {
+      let videoElement = document.getElementById('videojs-panorama-player');
+      var width = window.innerWidth;
+      var height = window.innerHeight;
+      that.player = videojs(videoElement, {}, () => {});
+      that.player.src({ src: source, type: 'video/mp4' });
+      that.player.width(width), that.player.height(height);
 
-        panorama(that.player, {
-          autoMobileOrientation: true,
-          initFov: 100,
-          initLat: -5,
-          initLon: -180,
-          showNotice: false
-        });
+      panorama(that.player, {
+        autoMobileOrientation: true,
+        initFov: 100,
+        initLat: -5,
+        initLon: -180,
+        showNotice: false
+      });
 
-        that.player.on('VRModeOn', function() {
-          that.player.controlBar.fullscreenToggle.trigger('tap');
-        });
-      }, 500);
+      that.player.on('VRModeOn', function() {
+        that.player.controlBar.fullscreenToggle.trigger('tap');
+      });
+    }, 500);
     // });
+  }
+
+  goBack() {
+    window.history.back();
   }
 }
